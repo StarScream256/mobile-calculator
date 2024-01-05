cc0596816b11ab198ababcdb768975dd11bec43f
Arif <muhammadarifhermawan4@gmail.com>
2024-01-05 13:47:14 +0700

    FEAT:add calulator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,18 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText passwordInput;
+    private ImageView toggleIcon;
+    private boolean passwordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        passwordInput = findViewById(R.id.passwordInput);
+        toggleIcon = findViewById(R.id.togglePasswordIconLogin);
     }
 
     public void navigateToRegistration(View view) {
         Intent registration = new Intent(MainActivity.this, RegistrationActivity.class);
         startActivity(registration);
+    }
+
+    public void togglePasswordLogin(View view) {
+        passwordVisible = !passwordVisible;
+
+        int drawableId = (passwordVisible) ? R.drawable.eye_slash_svgrepo_com : R.drawable.eye_svgrepo_com;
+        toggleIcon.setImageResource(drawableId);
+
+        int inputType = (passwordVisible) ?
+                            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD :
+                            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+        passwordInput.setInputType(inputType);
+        passwordInput.setSelection(passwordInput.length());
     }
 }
--- app/src/main/java/com/example/calculator/RegistrationActivity.java
+++ app/src/main/java/com/example/calculator/RegistrationActivity.java
@@ -5,18 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText passwordInput;
+    private ImageView toggleIcon;
+    private boolean passwordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+
+        passwordInput = findViewById(R.id.passwordInput);
+        toggleIcon = findViewById(R.id.togglepasswordIconRegistration);
     }
 
     public void navigateToLogin(View view) {
         Intent login = new Intent(RegistrationActivity.this, MainActivity.class);
         login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(login);
+    }
+
+    public void togglePasswordRegistration(View view) {
+        passwordVisible = !passwordVisible;
+
+        int drawableId = (passwordVisible) ? R.drawable.eye_slash_svgrepo_com : R.drawable.eye_svgrepo_com;
+        toggleIcon.setImageResource(drawableId);
+
+        int inputType = (passwordVisible) ?
+                            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD :
+                            InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+        passwordInput.setInputType(inputType);
+
+        passwordInput.setSelection(passwordInput.length());
+
     }
 }
